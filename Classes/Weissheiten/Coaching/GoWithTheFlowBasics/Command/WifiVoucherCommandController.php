79d5de4190fd81a8d89306b8280fa2da07d48ef6
Florian Weiss <florian.weiss@weissheiten.at>
2016-08-04 11:35:14 +0200

    introduce first draft of command controller for inserting Wifi vouchers

---
+++ Classes/Weissheiten/Coaching/GoWithTheFlowBasics/Command/WifiVoucherCommandController.php
@@ -0,0 +1,28 @@
+<?php
+namespace Weissheiten\Coaching\GoWithTheFlowBasics\Command;
+
+/*
+ * This file is part of the Weissheiten.Coaching.GoWithTheFlowBasics package.
+ */
+
+use TYPO3\Flow\Annotations as Flow;
+
+/**
+ * @Flow\Scope("singleton")
+ */
+class WifiVoucherCommandController extends \TYPO3\Flow\Cli\CommandController{
+
+    /**
+     * WiFi Code creation via command line
+     *
+     * Adds a new voucher to the database with the passed credentials
+     *
+     * @param string $username - This argument is required
+     * @param string $password - This argument is required
+     * @return void
+     */
+    public function insertCommand($username, $password){
+        $this->outputLine("Username: $username, Password: $password");
+    }
+
+}
--- Classes/Weissheiten/Coaching/GoWithTheFlowBasics/Controller/StandardController.php
+++ Classes/Weissheiten/Coaching/GoWithTheFlowBasics/Controller/StandardController.php
@@ -9,14 +9,11 @@ use TYPO3\Flow\Annotations as Flow;
 
 class StandardController extends \TYPO3\Flow\Mvc\Controller\ActionController
 {
-
     /**
      * @return void
      */
     public function indexAction()
     {
-        \TYPO3\Flow\var_dump('demo');
-
         $this->view->assign('foos', array(
             'bar', 'baz'
         ));
